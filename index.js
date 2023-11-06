eaf7dd35cf3722b0dcd2ef63f5f1965f90028098
Yohann Andre <y.andre90000@gmail.com>
2023-11-06 16:06:45 +0100

    add port env

--- index.js
+++ index.js
@@ -453,27 +453,23 @@ app.get("/", (req, res) => {
     });
 });
 
-// Northflank va nous fournir une variable process.env.PORT
-// if (process.env.PORT) {
-//   app.listen(process.env.PORT, () => {
-//     console.log("Server started");
-//   });
-// } else {
-//   app.listen(3200, () => {
-//     console.log("Server started");
-//   });
-// }
+
 
 app.all('*', (req, res) => {
     res.status(400).json("This route does not exist")
 })
 
+// Northflank va nous fournir une variable process.env.PORT
 
-// app.listen(process.env.PORT || 3200, () => {
-//     console.log("Server started");
-// });
+if (process.env.PORT) {
+    app.listen(process.env.PORT, () => {
+        console.log("Server started");
+    });
+} else {
+    app.listen(3200, () => {
+        console.log("Server started");
+    });
+}
 
 
 
-
-app.listen(3000)
